919c614724fcec11d9fc7402090a4e187484a3cf
RoboGnome <26966675+RoboGnome@users.noreply.github.com>
2021-08-02 22:25:18 +0200

    Added a simpler example

--- src/main.c
+++ src/main.c
@@ -1,75 +1,40 @@
 /**
- *  Simple UART example using the sdk_support library on a thunderboard sense 2
+ *  Simple blinky example using the sdk_support library on a thunderboard sense 2
  * 
  */
 
-#include <stdlib.h>
 #include <stdint.h>
-#include "em_cmu.h"
-#include "em_gpio.h"
-#include "em_usart.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include "em_device.h"
 #include "em_chip.h"
+#include "em_cmu.h"
+#include "em_emu.h"
+#include "em_gpio.h"
 
+#define RED 8
+#define GREEN 9
 
-#define BUFFER_SIZE 80
-#define  HFXOFrequency 32000000UL
-
-
+// Default clock value (see p. 319 TRM)
+#define HFRCO_IN_HZ 1000000
+#define TIMER_1S 1*(HFRCO_IN_HZ)
 
 int main() 
 {
   CHIP_Init();
-  
-  // Enable oscillator to GPIO and USART2 modules
-  CMU_ClockEnable(cmuClock_GPIO, true);
-  CMU_ClockEnable(cmuClock_USART2, true);
 
-  // set pin modes for UART TX and RX pins
-  GPIO_PinModeSet(gpioPortA, 7, gpioModePushPull, 1);
-  GPIO_PinModeSet(gpioPortA, 6, gpioModeInput, 0);
+    CMU_ClockEnable(cmuClock_GPIO, true);
 
-  // Initialize USART asynchronous mode and route pins
-  USART_InitAsync_TypeDef init = {  usartEnable,           /* Enable RX/TX when initialization is complete. */                     \
-                                    0,                     /* Use current configured reference clock for configuring baud rate. */ \
-                                    9600,                /* 9600 bits/s. */                                                    \
-                                    usartOVS16,            /* 16x oversampling. */                                                 \
-                                    usartDatabits8,        /* 8 data bits. */                                                      \
-                                    usartNoParity,         /* No parity. */                                                        \
-                                    usartStopbits1,        /* 1 stop bit. */                                                       \
-                                    false,                 /* Auto CS functionality enable/disable switch */                       \
-                                  };
-  USART_InitAsync(USART2, &init);
-  USART2->ROUTELOC0 |= (USART_ROUTELOC0_RXLOC_LOC0) | USART_ROUTELOC0_TXLOC_LOC2;
-  USART2->ROUTEPEN |= USART_ROUTEPEN_TXPEN | USART_ROUTEPEN_RXPEN;
+    // Initialize LED driver
+    GPIO_PinModeSet(gpioPortD, RED, gpioModePushPull, 0);
+    GPIO_PinOutSet(gpioPortD, RED);
 
-  int i,j;
-  char txBuffer[] = "Hello World";
-  for (i = 0 ; txBuffer[i] != 0; i++)
-  {
-    USART_Tx(USART2, txBuffer[i]);
-  }
-
-  char buffer[BUFFER_SIZE];
-  while (1) 
-  {
-
-    // Read a line from the UART
-    for (i = 0; i < BUFFER_SIZE - 1 ; i++ )
+    while (1)
     {
-      buffer[i] = USART_Rx(USART2);
-      if (buffer[i] == '\r')
-      {
-        break; // Breaking on CR prevents it from being counted in the number of bytes
-      }
+        // Ca. 1s blinking, depending on the oscillator setting
+        for (int i = 0; i < TIMER_1S; i++)
+        {
+        }
+        GPIO_PinOutToggle(gpioPortD, RED);
     }
-
-    // Echo the line back, adding CRLF
-    for (j = 0; j < i ; j ++ )
-    {
-      USART_Tx(USART2, buffer[j]);
-    }
-
-    USART_Tx(USART2, '\r');
-    USART_Tx(USART2, '\f');
-  }
 }
